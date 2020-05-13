f55176ad30ff82c75521e84621440f6fa0c32e33
deepak11du <deepak.mca19.du@gmail.com>
2020-05-13 11:59:05 +0530

    adding location trail and database storage

--- static/js/loadmap.js
+++ static/js/loadmap.js
@@ -2,7 +2,21 @@ var x = document.getElementById("info");
 let plat = -1;
 let plon = -1;
 let currentTimestamp ;
+let path = [];
 
+
+let db;
+let map;
+let request;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 28, lng: 77 },
+        zoom: 8
+    });
+    console.log("map printed: " + plat + "  " + plon);
+}
+
+console.log("declaring variables",plat,plon);
 function getLocation() {
 
     currentTimestamp = new Date();
@@ -15,15 +29,57 @@ function getLocation() {
     }
 }
 
+function displayData(){
+    path = [];
+    //console.log('hello');
+    let objectStore = db.transaction('trail_os').objectStore('trail_os');
+    objectStore.openCursor().onsuccess = function(e){
+        let cursor = e.target.result;
+
+        if(cursor){
+            //console.log(cursor.value);
+            //console.log(cursor);
+            let userLatLng = new google.maps.LatLng(cursor.value.latitude, cursor.value.longitude);
+            //console.log(userLatLng);
+            let item = {lat: cursor.value.latitude,lng: cursor.value.longitude};
+            //console.log(item);
+            path.push(item);
+            let marker = new google.maps.Marker({
+                position: userLatLng,
+                title: "Time: "+ cursor.value.timestamp.toLocaleTimeString() +", "+ cursor.value.timestamp.toDateString(),
+                map: map
+            });
+            //console.log(marker.getPosition());
+            map.setCenter(marker.getPosition());
+            map.setZoom(17);
+            map.panTo(marker.position);
+            cursor.continue();
+        }
+        var poly = new google.maps.Polyline({
+            path: path,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+        poly.setMap(map);
+        if(path.length >0){
+            console.log("setting previous known lat lon from db",path[path.length-1].lat,path[path.length-1].lng);
+            plat = path[path.length-1].lat;
+            plon = path[path.length-1].lng;
+        }
+    }
+    
+}
+
 function showPosition(position) {
     
     if(plat == -1){
-        
+        console.log("no item fetched from db yet");
         plat = position.coords.latitude;
         plon = position.coords.longitude;
         //initMap();
         var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        
         var marker = new google.maps.Marker({
             position: userLatLng,
             title: "Time: "+ currentTimestamp.toLocaleTimeString() +", "+ currentTimestamp.toDateString(),
@@ -34,9 +90,34 @@ function showPosition(position) {
         map.panTo(marker.position);
         acc = position.coords.accuracy;
         x.innerHTML = "Time: "+ currentTimestamp.toLocaleTimeString() +", "+ currentTimestamp.toDateString()+ "<br>"+"Latitude: " + plat +"<br>Longitude: " + plon+"<br>accuracy: "+acc;
+    
+        let newItem = {
+            timestamp: currentTimestamp,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+
+        let transaction = db.transaction(['trail_os'],'readwrite');
+        let objectStore = transaction.objectStore('trail_os');
+
+        let request = objectStore.add(newItem);
+        request.onsuccess = function(){
+            console.log('added first item in db successfully' + newItem.latitude+ " "+ newItem.longitude);
+        }
+        transaction.oncomplete = function() {
+            console.log('Transaction completed: database modification finished.');
+        
+            displayData();
+        };
+
+        transaction.onerror = function() {
+            console.log('Transaction not opened due to error');
+          };
     }
-    else if(calcdistance(plat,plon,position.coords.latitude,position.coords.longitude)>1){
+    else if(calcdistance(plat,plon,position.coords.latitude,position.coords.longitude)>10){
         //initMap();
+        plat = position.coords.latitude;
+        plon = position.coords.longitude;
         acc = position.coords.accuracy;
         var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         
@@ -49,7 +130,34 @@ function showPosition(position) {
         map.setZoom(17);
         map.panTo(marker.position);
         x.innerHTML ="Time: "+ currentTimestamp.toLocaleTimeString()+", "+currentTimestamp.toDateString()+"<br>Latitude: " + position.coords.latitude +"<br>Longitude: " + position.coords.longitude+"<br>accuracy: "+acc;
+    
+        let newItem = {
+            timestamp: currentTimestamp,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+
+        let transaction = db.transaction(['trail_os'],'readwrite');
+        let objectStore = transaction.objectStore('trail_os');
+
+        let request = objectStore.add(newItem);
+        request.onsuccess = function(){
+            console.log('added item in db successfully' + newItem.latitude+" "+ newItem.longitude);
+        }
+        transaction.oncomplete = function() {
+            console.log('Transaction completed: database modification finished.');
+        
+            displayData();
+        };
+
+        transaction.onerror = function() {
+            console.log('Transaction not opened due to error');
+          };
     }
+    else{
+        x.innerHTML ="Time: "+ currentTimestamp.toLocaleTimeString()+", "+currentTimestamp.toDateString()+"<br>Latitude: " + position.coords.latitude +"<br>Longitude: " + position.coords.longitude+"<br>accuracy: "+position.coords.accuracy;
+    }
+    
     
 }
 function calcdistance(lat1,lon1,lat2,lon2){
@@ -67,13 +175,38 @@ function calcdistance(lat1,lon1,lat2,lon2){
 
 }
 
-setInterval(getLocation,5000);
 
-let map;
-function initMap(){
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 28, lng: 77},
-        zoom: 8
-    });
-    console.log("map printed: "+plat+"  "+plon);
-}
+window.onload = function(e){
+    request = window.indexedDB.open('trail_os',1);
+
+    request.onerror = function(){
+        console.log('database failed to open');
+    };
+
+    request.onsuccess = function(){
+        console.log('database opened successfully');
+        db = request.result;
+        setInterval(getLocation,5000);
+
+        displayData();
+        console.log("displaying data...");
+    };
+
+    request.onupgradeneeded = function(e){
+
+        let db = e.target.result;
+
+        let objectStore = db.createObjectStore('trail_os',{keyPath: 'trail_id',autoIncrement: true});
+
+        objectStore.createIndex('latitude','latitude',{unique: false});
+        objectStore.createIndex('timestamp','timestamp',{unique: true});
+        objectStore.createIndex('longitude','longitude',{unique: false});
+
+        console.log('database setup complete');
+
+    };
+
+    
+};
+
+
